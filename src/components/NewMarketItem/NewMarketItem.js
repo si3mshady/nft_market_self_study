f1936ce52198419cfc9d301a9c08fdafdb2b278e
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-01 14:30:45 -0500

    save point

--- src/components/NewMarketItem/NewMarketItem.js
+++ src/components/NewMarketItem/NewMarketItem.js
@@ -3,17 +3,7 @@ import React from 'react'
 import './NewMarketItem.css'
 
 
-const handleSubmit = () => {
-
-}
-
-const handleChange = (e) => {
-    e.preventDefault()
-  
-  }
-
 export default function NewMarketItem() {
-
 
     const [formData, setFormData] = React.useState({
         address: "",
@@ -22,18 +12,30 @@ export default function NewMarketItem() {
         fee: "",
         appointmentType: ""
       })
-      
-    return (
-        <div class="container">
+
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
        
-            <form >
+      }
+      
+      const handleChange = (e) => {
+        e.preventDefault()
+        setFormData({...formData, [e.target.name]: e.target.value})
+        console.log(`This is the new value of ${formData.address}`)
+      
+      }
+
+    return (+
+    
+          <form  onSubmit={handleSubmit}>
             <h1>For Providers: Please create a new appointment token</h1>
             <input placeholder='Doctor Wallet Address'  name='address' type='text' onChange={handleChange} />
-          <input placeholder='Set Appointment Date'  name='date' type='text' onChange={handleChange} />
-          <input placeholder='Appointment Type' name='appointmentType' type='text' onChange={handleChange} />
-          <input placeholder='Doctor Email' name='email' type='text' onChange={handleChange} />
-          <input placeholder='Doctor consultation fee' name='fee' type='text' onChange={handleChange}/>         
-         <input type='submit'/>
+            <input placeholder='Set Appointment Date'  name='date' type='text' onChange={handleChange} />
+            <input placeholder='Appointment Type' name='appointmentType' type='text' onChange={handleChange} />
+            <input placeholder='Doctor Email' name='email' type='text' onChange={handleChange} />
+            <input placeholder='Doctor consultation fee' name='fee' type='text' onChange={handleChange}/>         
+            <input type='submit'/>
         </form>
         </div>
     )
