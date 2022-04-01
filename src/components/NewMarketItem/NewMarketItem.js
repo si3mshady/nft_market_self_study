f959caa1451ddad92f083afe3039cdf0fe34aac8
Elliott Arnold <si3mshady@Elliotts-MacBook-Pro.local>
2022-04-01 09:19:58 -0500

    updated form

--- src/components/NewMarketItem/NewMarketItem.js
+++ src/components/NewMarketItem/NewMarketItem.js
@@ -25,17 +25,16 @@ export default function NewMarketItem() {
       
     return (
         <div class="container">
+       
             <form >
-
-                <label>Doctor Wallet Address</label>
-                <input type="text"  name="address" placeholder="ETH wallet address"/>
-
-                <label>Appt Date</label>
-                <input type="text" name="date" placeholder="Appt Date"/>
-
-                <input type="submit" value="Submit"/>
-
-            </form>
+            <h1>For Providers: Please create a new appointment token</h1>
+            <input placeholder='Doctor Wallet Address'  name='address' type='text' onChange={handleChange} />
+          <input placeholder='Set Appointment Date'  name='date' type='text' onChange={handleChange} />
+          <input placeholder='Appointment Type' name='appointmentType' type='text' onChange={handleChange} />
+          <input placeholder='Doctor Email' name='email' type='text' onChange={handleChange} />
+          <input placeholder='Doctor consultation fee' name='fee' type='text' onChange={handleChange}/>         
+         <input type='submit'/>
+        </form>
         </div>
     )
 }
