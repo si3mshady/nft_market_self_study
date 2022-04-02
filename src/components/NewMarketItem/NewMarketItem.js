7418051539137086baf3cfc9d116954111a6b01e
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-02 17:05:12 -0500

    working on QRC code generation

--- src/components/NewMarketItem/NewMarketItem.js
+++ src/components/NewMarketItem/NewMarketItem.js
@@ -13,22 +13,51 @@ export default function NewMarketItem() {
         appointmentType: ""
       })
 
+    const [uri, setEncodedUri] = React.useState('')
+
+     const [loading, setLoadingState] = React.useState(false)
+
       const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(formData)
+        setLoadingState(!loading)
+        stringify_to_base64()
        
       }
       
       const handleChange = (e) => {
         e.preventDefault()
         setFormData({...formData, [e.target.name]: e.target.value})
-        console.log(`This is the new value of ${formData.address}`)
+        
       
       }
 
-    return (+
-    
-          <form  onSubmit={handleSubmit}>
+      const encodeURI = () => {
+
+        const uri =  `https://api.qrserver.com/v1/create-qr-code/?data=${stringify_to_base64()}`
+        
+        // const uri = encodeURI(uri)
+        console.log(uri)
+        setEncodedUri(uri)
+        
+      }
+
+
+      const stringify_to_base64  = () => {
+        const stringified_json = JSON.stringify(formData)
+        const converted_base64 = btoa(stringified_json)
+        console.log(converted_base64)
+        encodeURI()
+        // return converted_base64
+
+      }
+
+   
+    return (
+
+      <>
+
+        {!loading ?
+         (<form  onSubmit={handleSubmit}>
             <h1>For Providers: Please create a new appointment token</h1>
             <input placeholder='Doctor Wallet Address'  name='address' type='text' onChange={handleChange} />
             <input placeholder='Set Appointment Date'  name='date' type='text' onChange={handleChange} />
@@ -36,7 +65,22 @@ export default function NewMarketItem() {
             <input placeholder='Doctor Email' name='email' type='text' onChange={handleChange} />
             <input placeholder='Doctor consultation fee' name='fee' type='text' onChange={handleChange}/>         
             <input type='submit'/>
-        </form>
-        </div>
+        </form>): 
+        
+        (
+          <div className='container_qrc'>
+            
+          <img src={uri} alt='' title='' />
+          <button onClick={()=> {setLoadingState(!loading)}} className='submitButton' type='submit'> Submit </button> 
+          </div>
+                   
+        )
+     
+     
+        
+        
+}
+      </>
+        
     )
 }
